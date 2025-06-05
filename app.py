c23be271cf7ca2272390ac1025bda2f6147dff20
danielgreenhouse <dmgreenhouse@outlook.com>
2025-06-05 10:39:41 +0100

    Update MW -> W

--- app.py
+++ app.py
@@ -2,26 +2,27 @@
 import streamlit as st
 import traceback
 import time
+from numpy import log10
 from models.two_point_model import edge_simulator
 
 st.title("Tokamak Erosion Lifetime Simulator")
 
+MEGA = 1.e6
+TIME_DELAY = 5  # s
 # Log-scale input
 log10_p_in = st.number_input(
-    "Log₁₀(Input Power [MW])", 
-    value=0.0,
+    "Log₁₀(Input Power [W])", 
+    value=5.0,
     step=0.1,
-    min_value=-1.0,
-    max_value=4.0,
-    format="%.1f"
+    min_value=5.0,
+    max_value=10.0,
+    format="%.2f"
 )
 
 # Convert and show actual power (linear scale)
 p_in = 10 ** log10_p_in
-st.markdown(f"**Input Power:** {p_in:.2f} MW")
+st.markdown(f"**Input Power:** {p_in / MEGA:.3f} MW")
 
-MEGA = 1.e6
-TIME_DELAY = 5  # s
 
 # Reserve a spot for the GIF or result
 placeholder = st.empty()
@@ -36,11 +37,14 @@ if st.button("Run Experiment"):
         time.sleep(TIME_DELAY)
 
         # Run your model
-        erosion_lifetime = edge_simulator(power=p_in / MEGA)
+        erosion_lifetime = edge_simulator(power=p_in)
 
         # Replace GIF with result
         placeholder.empty()
-        st.success(f"Erosion Lifetime: {erosion_lifetime:.2f} years")
+        # st.success(f"Erosion Lifetime:\n {erosion_lifetime:.2f} years\n Log₁₀(Erosion Lifetime [years])={log10(erosion_lifetime):.2f}")
+        st.success(
+            f"""Erosion Lifetime: {erosion_lifetime:.3f} years""")
+        # Log₁₀(Erosion Lifetime [years]): {log10(erosion_lifetime):.2f}""")
 
     except Exception as e:
         placeholder.empty()
